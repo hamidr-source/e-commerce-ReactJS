818edbcb3e908fa89768d779dec08aacf83e8201
hamidr-source <hr454837@gmail.com>
2025-07-18 13:03:02 +0330

    feat: implement routing and add HomePage component with loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PageLoader from "./components/Loaders/PageLoader.jsx";
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
+
 function App() {
-  return <div className="text-blue-500">Hello world</div>;
+  return (
+    <Router>
+      <Suspense fallback={<PageLoader />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
 }
 
 export default App;
